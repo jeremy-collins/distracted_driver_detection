80c4e7dcde701ab497a527f8f6f820b873f8509e
Jeremy Collins <47015212+jeremy-collins@users.noreply.github.com>
2022-04-19 13:20:12 -0400

    Add files via upload

--- split_folder.py
+++ split_folder.py
@@ -2,6 +2,10 @@ import pandas as pd
 import os
 import shutil
 
+
+# Run this script with: "sudo -E python3 split_folder.py"
+
+# CHANGE TO YOUR COMBINED DATASET DIRECTORY
 img_dir = '/media/jer/data2/state-farm-distracted-driver-detection/imgs/train/combined/'
 df = pd.read_csv('/media/jer/data2/state-farm-distracted-driver-detection/driver_imgs_list.csv')
 
@@ -13,13 +17,17 @@ num_samples = img_names.shape[0]
 
 for i, img_name in enumerate(img_names):
     if int(people[i][1:]) <= 56:
-        shutil.copy("/media/jer/data2/state-farm-distracted-driver-detection/imgs/train/combined/" + img_name, "/media/jer/data2/state-farm-distracted-driver-detection/imgs/train/combined/train/" + img_name)
-        print("copying to train " + img_name)
-        print("train", int(people[i][1:]))
+        shutil.copy(img_dir + img_name, img_dir + "train/" + img_name)
+        print("copying to train " + img_name, int(people[i][1:]))
     elif int(people[i][1:]) > 56:
-        # shutil.copy("/media/jer/data2/state-farm-distracted-driver-detection/imgs/train/combined/" + img_name, "/media/jer/data2/state-farm-distracted-driver-detection/imgs/train/combined/test/" + img_name)
-        print("not copying to test " + img_name)
-        print("test", int(people[i][1:]))
+        shutil.copy(img_dir + img_name, img_dir + "test/" + img_name)
+        print("copying to test " + img_name, int(people[i][1:]))
+        
+        
+num_train = len(os.listdir(img_dir + "/train/"))
+num_test = len(os.listdir(img_dir + "/test/"))
 
+print("train ratio: ", num_train / num_samples)
+print("test ratio: ", num_test / num_samples)
         
         
